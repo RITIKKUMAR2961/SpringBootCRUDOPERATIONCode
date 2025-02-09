fb358a8805caf318da29ea08fa77703f601fe3d2
RITIK KUMAR <122879541+RITIKKUMAR2961@users.noreply.github.com>
2025-02-09 17:04:09 +0530

    Add files via upload

---
+++ ServerSideCrudOperationsWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.RITIKKUMAR.Server_Side.CRUD.Operations.with.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideCrudOperationsWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideCrudOperationsWithSpringBootApplication.class, args);
+	}
+
+}
